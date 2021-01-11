363da60b02b4afc04b4ad58919cf4061f5d84a21
Shaina C <scailles23@gmail.com>
2021-01-11 08:30:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Memorable::InstanceClass
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
