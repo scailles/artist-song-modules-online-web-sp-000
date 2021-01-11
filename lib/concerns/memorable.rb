ad560d2ebca676e5b7cfc8557d54bf56b4994655
Shaina C <scailles23@gmail.com>
2021-01-11 08:35:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,33 +1,17 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-module ClassMethods
+    def count
+      self.all.count
+    end
+  end
   
-def self.reset_all
-  self.all.clear
-end
-
-
-def self.count
-  self.all.count
-end
-
-end
-
-
-module InstanceMethods
-   
+  module InstanceMethods
     def initialize
       self.class.all << self
     end
-    
-end
-
-
-
-
-
-
-
-
-
+  end
 end
